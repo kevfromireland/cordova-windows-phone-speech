2040bae36e32ea5e04c609d7075cf3036700da9d
Kevin Boyle <kevin.boyle@red-gate.com>
2014-05-29 21:58:36 +0100

    think this gets wrapped up by plugman

--- www/speech.js
+++ www/speech.js
@@ -1,14 +1,10 @@
-﻿cordova.define("org.apache.cordova.device.device", function (require, exports, module) {
-
-    var exec = require('cordova/exec');
-    function Speech() {
+﻿var exec = require('cordova/exec');
+function Speech() {
         alert('created');
-    }
+}
 
-    Speech.prototype.speak = function (text, successCallback, errorCallback) {
-        
-        exec(successCallback, errorCallback, "ie.kevinboyle.cordova.speech", "speek", [text]);
-    };
+Speech.prototype.speak = function (text, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, "ie.kevinboyle.cordova.speech", "speek", [text]);
+};
 
-    module.exports = new Speech();
-});
+module.exports = new Speech();
