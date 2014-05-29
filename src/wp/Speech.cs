db2fb3b08e763128cbc72b3667ca70923c2610d8
Kevin Boyle <kevin.boyle@red-gate.com>
2014-05-29 21:47:04 +0100

    Basic TTS plugin

---
+++ src/wp/Speech.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPCordovaClassLib.Cordova.Commands;
+
+namespace ie.kevinboyle.cordova.speech
+{
+    internal class Speech : BaseCommand
+    {
+        public void speak(string options)
+        {
+
+        }
+    }
+}
---
+++ www/speech.js
@@ -0,0 +1,14 @@
+﻿cordova.define("org.apache.cordova.device.device", function (require, exports, module) {
+
+    var exec = require('cordova/exec');
+    function Speech() {
+        alert('created');
+    }
+
+    Speech.prototype.speak = function (text, successCallback, errorCallback) {
+        
+        exec(successCallback, errorCallback, "ie.kevinboyle.cordova.speech", "speek", [text]);
+    };
+
+    module.exports = new Speech();
+});
