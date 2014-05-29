6a6e9aa4fa35b9bf74a43e7f90af2032e7fba2e7
Kevin Boyle <kevin.boyle@red-gate.com>
2014-05-29 22:13:44 +0100

    Update some ids

--- www/speech.js
+++ www/speech.js
@@ -4,7 +4,7 @@ function Speech() {
 }
 
 Speech.prototype.speak = function (text, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Speech", "speak", [text]);
+    exec(successCallback, errorCallback, "ie.kevinboyle.cordova.speech.Speech", "speak", [text]);
 };
 
 module.exports = new Speech();
